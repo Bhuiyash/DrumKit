0778532356de7c5bffd22913dd7e3e4e425a8934
Bhuiyash Kumar <92365534+Bhuiyash@users.noreply.github.com>
2022-11-15 13:05:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  // var tomno=i ;
+  // var newSource="sounds/tom-"+tomno+".mp3";
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttoninnerhtml = this.innerHTML;
+    forsound(buttoninnerhtml);
+  });
+
+
+}
+
+document.addEventListener("keypress", function() {
+  forsound(event.key);
+
+});
+
+
+function forsound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    default:
+
+  }
+
+}
